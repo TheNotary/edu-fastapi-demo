0e0a4736665ce21d35b541895331d72033a1f465
TheNotary <no@mail.plz>
2023-06-22 16:38:16 -0500

    refactors semantic module to use a good template well

--- static/semantic/semantic_router.py
+++ static/semantic/semantic_router.py
@@ -1,17 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 from transformers import pipeline
 from pydantic import BaseModel, Field
+import pdb
+
+router = APIRouter()
 
 model_name = 'distilbert-base-uncased-finetuned-sst-2-english'
 
 class InputData(BaseModel):
     input_data: str = Field(..., min_length=1, description="Input data must be at least 1 character long")
 
-router = APIRouter()
 
-@router.post("/")
+templates = Jinja2Templates(directory="templates")
+
+@router.get("")
+async def semantic(request: Request):
+    # pdb.set_trace() # for debugging
+    return templates.TemplateResponse("semantic.html", {
+        "request": request })
+
+@router.post("")
 async def semantic(json: InputData):
     output = pipeline(task = 'sentiment-analysis',
                       model = model_name)(json.input_data)[0]
