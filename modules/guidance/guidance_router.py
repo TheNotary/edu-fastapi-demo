fd9886a5ef9100f77e36e355d5a0d1c8ae66b766
TheNotary <no@email.plz>
2023-07-05 08:59:08 -0500

    fleshing out guidance module

--- modules/guidance/guidance_router.py
+++ modules/guidance/guidance_router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, Field
 from helpers.jinja_helpers import build_templates_and_router
+import guidance
 
 templates, router, module_name = build_templates_and_router(__file__)
+
+llm = guidance.llms.Transformers("wizardlm-30b-uncensored.ggmlv3.q6_K.bin", device=0)
 
 class InputData(BaseModel):
     input_data: str = Field(..., min_length=1, description="Input data must be at least 1 character long")
